44da134f9c8421a12742018be73f57aae36b87fb
lesliekimm <leslie.kimm@gmail.com>
2015-04-21 10:39:32 -0400

    Update Owl.java

--- src/BooleanForest/Owl.java
+++ src/BooleanForest/Owl.java
@@ -1,17 +1,12 @@
 package BooleanForest;
 
-import java.awt.Graphics;
-
-import javax.swing.JComponent;
-
-@SuppressWarnings("serial")
-public class Owl extends JComponent {
+public class Owl {
 	// Private static final members of Owl class are Strings that
-	// will determine the size of the Owl image.
-	private static final String BOB = "BOB";
-	private static final String ALICE = "ALICE";
-	private static final String CHLOE = "CHLOE";
-	private static final String DAVID = "DAVID";
+	// indicate which character the Owl is.
+	private static final String BOB = "BOB";			// 158 x 159
+	private static final String ALICE = "ALICE";		// 158 x 159
+	private static final String CHLOE = "CHLOE";		// 106 x 107
+	private static final String DAVID = "DAVID";		// 106 x 107
 	
 	// Private members of the Owl class:
 	private int xCoord;						// x-coordinate of Owl
@@ -58,11 +53,5 @@ public class Owl extends JComponent {
 	// Getter: return size
 	public String getOwl() {
 		return name;
-	}
-	
-	@Override
-	public void paintComponent(Graphics panel) {
-		super.paintComponent(panel);			// Override super method
-
 	}
 }
