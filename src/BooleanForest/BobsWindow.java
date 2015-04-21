c0a6e1678227a05418ad7e32a294a2ed2a3b9756
Leslie Kim <leslie.kimm@gmail.com>
2015-04-21 10:36:47 -0400

    edits to Owl and Bobs Window

--- src/BooleanForest/BobsWindow.java
+++ src/BooleanForest/BobsWindow.java
@@ -1,6 +1,8 @@
 package BooleanForest;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,31 +12,89 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class BobsWindow extends JPanel implements ActionListener {
-	// Private members of BobsWindow class:
-	private String instructions;
-	private GameButton gameButton;
+	// Private static final members of BobsWindow class:
+	private static final int BOBS_WINDOW_WIDTH = 567;
+	private static final int BOBS_WINDOW_HEIGHT = 310;
+	private static final String LEVEL_1 = "Go to Level 1";
+	private static final String LEVEL_2 = "Go to Level 2";
+	private static final String LEVEL_3 = "Go to Level 3";
+	private static final String LEVEL_4 = "Go to Level 4";
+	private static final String LEVEL_5 = "Go to Level 5";
+	private static final String PLAY_LEVEL = "Play Level";
 	
-	public BobsWindow() {
+	// Private members of BobsWindow class:
+	private String instructions = "";
+	private String buttonMessage = "";
+	private GameButton gameButton;
+	private Game game = new Game();
+	
+	public BobsWindow(Game game, String instructions, String buttonMessage) {
+		this.game = game;
+		this.instructions = instructions;
+		this.buttonMessage = buttonMessage;
+		gameButton = new GameButton(buttonMessage, "white");
 		
+		initBobsWindow();
 	}
 	
-	public BobsWindow(String instructions) {
-		this.instructions = instructions;
+	public void initBobsWindow() {
+		setPreferredSize(new Dimension(BOBS_WINDOW_WIDTH, BOBS_WINDOW_HEIGHT));				// Set size of SidePanel
+		setLayout(new BorderLayout());
+		
+		gameButton.addActionListener(this);
 	}
 	
 	public void setInstructions(String instructions) {
 		this.instructions = instructions;
 	}
 	
+	public void setButtonMessage(String buttonMessage) {
+		this.buttonMessage = buttonMessage;
+	}
+	
+	public String getInstructions() {
+		return instructions;
+	}
+	
+	public String getButtonMessage() {
+		return buttonMessage;
+	}
+	
 	@Override
-	public void paintComponent(Graphics panel) {
-		super.paintComponent(panel);			// Override super method
-
+	public void paintComponent(Graphics g) {
+		
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent event) {
+		if (event.getSource() == gameButton) {
+			String buttonMessage = gameButton.getText();
+			if (buttonMessage == LEVEL_1) {	
+//				pause.setText("Play");											// Set the text to "Play"
+//				game.pause();													// Pause the game
+			}
+			else if (buttonMessage == LEVEL_2) {
+				
+			}
+			else if (buttonMessage == LEVEL_3) {
+				
+			}
+			else if (buttonMessage == LEVEL_4) {
+				
+			}
+			else if (buttonMessage == LEVEL_5) {
+				
+			}
+			else if (buttonMessage == PLAY_LEVEL) {
+				
+			}
+			else {
+				// throw error
+			}
+		}
+		else {
+			// throw exception
+		}
 		
 	}
 
