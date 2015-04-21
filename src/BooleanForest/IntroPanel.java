7604587f49ffc92b1682ef1e5f13e419974dc4eb
alaysiaye <clairesbirthdaypresent@gmail.com>
2015-04-21 08:38:22 -0400

    JLH -- comments & deleting duplicate files from before restructure

--- src/BooleanForest/IntroPanel.java
+++ src/BooleanForest/IntroPanel.java
@@ -10,33 +10,57 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class IntroPanel extends JPanel {
 	
-	private static final String INSTRUCTION_TEXT = "Welcome to the Forest of Boolean Logic! My name is Bob. What is your name?";
-	private static final String text = "d";
+	private static final String INSTRUCTION_TEXT = "Welcome to the Forest of Boolean Logic! My name is Bob. Let's go on an adventure!";
 	
 	Game theGame;
 	
+	/**
+	 * IntroPanel()
+	 * 
+	 * Constructor, calls initIntro()
+	 * 
+	 * @param game
+	 */
 	public IntroPanel(Game game) {
 		initIntro(game);
 	}
 	
+	/**
+	 * initIntro()
+	 * 
+	 * Initializes intro screen, with the game, an instruction Bob's window, calls repaint
+	 * 
+	 * @param game
+	 */
 	private void initIntro(Game game) {
+		// Set private game to the current game
 		theGame = game;
 		
+		// Set default settings
 		setPreferredSize(new Dimension(640,480));
 		setDoubleBuffered(true);
 		
+		// Create and add instruction Bob's window
 		BobsWindow introInstructions = new BobsWindow(INSTRUCTION_TEXT);
 		add(introInstructions);
 		
-		GameButton startButton = new GameButton();
-		add(startButton);
-		
+		// Repaint screen
 		repaint();
 	}
 	
+	/**
+	 * paintComponenent()
+	 * 
+	 * Overrides paintComponenent(); draws background image
+	 * 
+	 * @param graphic
+	 */
 	@Override
 	public void paintComponent(Graphics g) {
+		// Call super
 		super.paintComponent(g);
+		
+		// Create new image icon, draw the image
 		Image image = new ImageIcon("Images/IntroScreen.jpg").getImage();
 		g.drawImage(image, 0, 0, null);
 	}
