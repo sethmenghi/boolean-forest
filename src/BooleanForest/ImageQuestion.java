e0112a02dd7879d1f3ab63dea0a35751818c4a67
Seth Menghi <sethmenghi@gmail.com>
2015-04-22 15:07:56 -0400

    Added owls to questions, going to try and drag them around

--- src/BooleanForest/ImageQuestion.java
+++ src/BooleanForest/ImageQuestion.java
@@ -1,8 +1,14 @@
 package BooleanForest;
 import java.lang.String;
 import java.util.ArrayList;
+
+import javax.swing.BorderFactory;
 import javax.swing.JComponent;
-import java.util.List;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.List;
 
 public class ImageQuestion extends JComponent implements Question {
 	String imageResourceOne;
@@ -10,43 +16,47 @@ public class ImageQuestion extends JComponent implements Question {
 	String imageResourceThree;
 	String imageResourceFour;
 
-	List<String> answerListOne;
-	List<String> answerListTwo;
+	List answerListOne;
+	List answerListTwo;
 
+	
 	@Override 
-	void paint(Graphic g){
-
+	public void paint(Graphics g){
+		
 	}
 
 	// Sets up the GUI for the question
 	@Override
-	void displayQuestion(){
+	public void displayQuestion(){
+		this.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 20));
+        this.setBorder(BorderFactory.createLineBorder(Color.GREEN, 2));
 		
+		setVisible(true);
 	}
 	
 	// Sets the question text
-	void setQuestion(String questionStringInput){
+	public void setQuestion(String questionStringInput){
 
 	}
 	
 	// Sets the different answer possibilities 
-	void setAnswerList(List answerListInput){
+	public void setAnswerList(List possibleAnswers, List correctAnwsers){
 
 	}
 	
 	// Determines if it is an image  or text question
-	void setQuestionLayout(String questionLayoutInput){
+	public void setQuestionLayout(String questionLayoutInput){
 
 	}
 	
 	// Submits the question after knowing if the user got the correct answer 
-	void submitQuestion(Boolean questionCorrectOrNot){
+	public void submitQuestion(Boolean questionCorrectOrNot){
 
 	}
 	
 	// Checks two objects against each other to check the answer 
-	void checkAnswer(Object submittedAnswer, Object correctAnswer){
-		if (submittedAnser != correctAnswer){
+	public void checkAnswer(Object submittedAnswer, Object correctAnswer){
+		if (submittedAnswer != correctAnswer){
 
 		}
 	}
