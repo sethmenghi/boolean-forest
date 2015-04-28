71255c565275393d8e5d8fa763df4f9c2a031cd1
Leslie Kim <leslie.kimm@gmail.com>
2015-04-28 15:28:15 -0400

    commented LevelOne

--- src/Levels/LevelOne.java
+++ src/Levels/LevelOne.java
@@ -1,11 +1,20 @@
 /**
  * LEVELONE CLASS
  * 
- * DESCRIPTION:
+ * DESCRIPTION: This class contains content for level one explanation
+ * and handles the content for the questions for level one.
  * 
  * IMAGE SOURCES:
- * http://yunus.hacettepe.edu.tr/~osert/Hotpot_2014_02/02_erdemli_batibay/coffee%20table.jpg
- * http://sweetclipart.com/multisite/sweetclipart/files/penguin_cute.png
+ * 1) http://sweetclipart.com/multisite/sweetclipart/files/penguin_
+ *    cute.png
+ * 2) http://yunus.hacettepe.edu.tr/~osert/Hotpot_2014_02/02_erdemli_
+ *    batibay/coffee%20table.jpg 
+ * 3) https://s-media-cache-ak0.pinimg.com/236x/6b/c5/37/
+ *    6bc537a241ffc746acb7d2180d2253d8.jpg
+ * 4) http://www.wpclipart.com/cartoon/animals/horse/horse_cartoon_
+ *    zebra.png
+ * 5) http://www.how-to-draw-funny-cartoons.com/image-files/cartoon-
+ *    flamingo-8.gif
  */
 
 package Levels;
@@ -21,6 +30,9 @@ import javax.swing.JOptionPane;
 import BooleanForest.Game;
 
 public class LevelOne {
+	// Declare private String for Bob resource:
+	private static final String BOB_SRC = "Images/Owls/Bob.png";		// Bob url
+	
 	// Declare static final Strings for explanations:
 	public static final String PAGE_TRUE = "TRUE";						// page 1	
 	public static final String PAGE_TRUE_A = "This is a penguin.";		// page 2
@@ -37,102 +49,120 @@ public class LevelOne {
 	public static final Image TABLE = new ImageIcon("Images/Levels/LevelOne/Explanation/Table.png").getImage();
 	public static final Image PINK = new ImageIcon("Images/Levels/LevelOne/Explanation/Pink.png").getImage();
 	
-	// https://s-media-cache-ak0.pinimg.com/236x/6b/c5/37/6bc537a241ffc746acb7d2180d2253d8.jpg
-	private static final String Q_ONE = "This is a monkey.";
+	// Declare static final Strings for question images:
 	private static final String IMAGE_ONE = "Images/Levels/LevelOne/Questions/Monkey.png";
-	private static final String Q_ONE_OPTION_1 = "True";
-	private static final String Q_ONE_OPTION_2 = "False";
-	private List<String> ANSWERS_ONE = new ArrayList<String>(); 	
-	
-	//http://www.wpclipart.com/cartoon/animals/horse/horse_cartoon_zebra.png
-	private static final String Q_TWO = "The zebra has stripes.";
 	private static final String IMAGE_TWO = "Images/Levels/LevelOne/Questions/Zebra.png";
-	private static final String Q_TWO_OPTION_1 = "True.";
-	private static final String Q_TWO_OPTION_2 = "False.";
-	private List<String> ANSWERS_TWO = new ArrayList<String>(); 	
-	
-	//http://www.how-to-draw-funny-cartoons.com/image-files/cartoon-flamingo-8.gif
-	private static final String  Q_THREE = "The flamingo is blue.";
 	private static final String IMAGE_THREE = "Images/Levels/LevelOne/Questions/Flamingo.png";
-	private static final String Q_THREE_OPTION_1 = "True.";
-	private static final String Q_THREE_OPTION_2 = "False.";
-	private List<String> ANSWERS_THREE = new ArrayList<String>(); 	
 	
-
-	private Game theGame;
-	private int currentQuestion = 1;
+	// Declare static final String for questions:
+	private static final String Q_ONE = "This is a monkey.";
+	private static final String Q_TWO = "The zebra has stripes.";
+	private static final String Q_THREE = "The flamingo is blue.";
 	
+	// Declare static final String for question answer choices:
+	private static final String ANSWER_TRUE = "True";
+	private static final String ANSWER_FALSE = "False";
+	
+	// Declare private members of the LevelOne class:
+	private Game theGame;											// to create reference to main game
+	private int currentQuestion = 1;								// current question being asked is 1
+	private List<String> ANSWERS_ONE = new ArrayList<String>(); 	// holds list of answers for question 1
+	private List<String> ANSWERS_TWO = new ArrayList<String>(); 	// holds list of answers for question 2
+	private List<String> ANSWERS_THREE = new ArrayList<String>();	// holds list of answers for question e
+	
+	/**
+	 * CONSTRUCTOR: This creates an instance of the LevelOne class
+	 * and creates a reference to the main game.
+	 * @param game
+	 */
 	public LevelOne(Game game) {
-		theGame = game;
+		this.theGame = game;										// creates a reference to the main game
 	}
 	
-	public void addPossibleAnswers(List<String> holdsPossibleAnswers, String option1, String option2, String option3, String option4){
-		holdsPossibleAnswers.add(option1);
-		holdsPossibleAnswers.add(option2);
-		holdsPossibleAnswers.add(option3);
-		holdsPossibleAnswers.add(option4);
-	}
-	
-	public void addPossibleAnswers(List<String> holdsPossibleAnswers, String option1, String option2){
-		holdsPossibleAnswers.add(option1);
-		holdsPossibleAnswers.add(option2);
-	}
-
+	/**
+	 * METHOD: This method is called from the LevelPanel for Level
+	 * 1 and calls the doQuestion method to go through the questions
+	 * for LevelOne class.
+	 * @param none
+	 */
 	public void startLevel() {
-		doQuestion();
+		doQuestion();												// call doQuestion
 	}
 	
-	public void nextQuestion(){
-		if (theGame.textQuestion.questionCompleted == true){
-			currentQuestion++;
-			doQuestion();
-		}
-		else{
-			JLabel warningImage = new JLabel(new ImageIcon("Images/Owls/Bob.png"));
-			JOptionPane.showMessageDialog(null, warningImage, "Question", JOptionPane.PLAIN_MESSAGE, null);
-		}
-	}
-	
+	/**
+	 * METHOD: This goes through and fills the Lists for each question
+	 * with the correct answer choices and sets the text question with
+	 * the proper content.
+	 * @param none
+	 */
 	public void doQuestion() {
 		switch (currentQuestion) {
+		// For the first question:
 		case 1:
-			if (ANSWERS_ONE.size() == 0){
-				addPossibleAnswers(ANSWERS_ONE,  Q_ONE_OPTION_1,  Q_ONE_OPTION_2);
-				theGame.textQuestion.setQuestion(Q_ONE);
-				theGame.textQuestion.setQuestionImage(IMAGE_ONE);
-				theGame.textQuestion.setAnswerList(ANSWERS_ONE,Q_ONE_OPTION_1);
-				theGame.textQuestion.initGui();
-				theGame.changeLayoutCard("TEXT_QUESTION");
-
+			if (ANSWERS_ONE.size() == 0) {
+				addPossibleAnswers(ANSWERS_ONE, ANSWER_TRUE, ANSWER_FALSE);		// fill answer choice list with choices
+				theGame.textQuestion.setQuestion(Q_ONE);						// set to first question
+				theGame.textQuestion.setQuestionImage(IMAGE_ONE);				// set image used for first question
+				theGame.textQuestion.setAnswerList(ANSWERS_ONE, ANSWER_TRUE);	// set the answer
+				theGame.textQuestion.initGui();									// call initGui form textQuestion class
+				theGame.changeLayoutCard("TEXT_QUESTION");						// change card layout to TextQuestion
 			}
 			break;
+		// For the second question:
 		case 2:	
-			if (ANSWERS_TWO.size() == 0){
-				addPossibleAnswers(ANSWERS_TWO,  Q_TWO_OPTION_1,  Q_TWO_OPTION_2);
-				theGame.textQuestion.setQuestion(Q_TWO);
-				theGame.textQuestion.setQuestionImage(IMAGE_TWO);
-				theGame.textQuestion.setAnswerList(ANSWERS_TWO,Q_TWO_OPTION_1);
-				theGame.textQuestion.initGui();
+			if (ANSWERS_TWO.size() == 0) {
+				addPossibleAnswers(ANSWERS_TWO, ANSWER_TRUE, ANSWER_FALSE);		// fill answer choice list with choices
+				theGame.textQuestion.setQuestion(Q_TWO);						// set to second question
+				theGame.textQuestion.setQuestionImage(IMAGE_TWO);				// set image used for second question
+				theGame.textQuestion.setAnswerList(ANSWERS_TWO,ANSWER_TRUE);	// set the answer
+				theGame.textQuestion.initGui();									// call initGui form textQuestion class
 			}
 			break;
+		// For the third question:
 		case 3:	
-			if (ANSWERS_THREE.size() == 0){
-				addPossibleAnswers(ANSWERS_THREE,  Q_THREE_OPTION_1,  Q_THREE_OPTION_2);
-				theGame.textQuestion.setQuestion(Q_THREE);
-				theGame.textQuestion.setQuestionImage(IMAGE_THREE);
-				theGame.textQuestion.setAnswerList(ANSWERS_THREE,Q_TWO_OPTION_2);
-				theGame.textQuestion.initGui();
+			if (ANSWERS_THREE.size() == 0) {
+				addPossibleAnswers(ANSWERS_THREE, ANSWER_TRUE, ANSWER_FALSE);	// fill answer choice list with choices
+				theGame.textQuestion.setQuestion(Q_THREE);						// set to third question
+				theGame.textQuestion.setQuestionImage(IMAGE_THREE);				// set image used for third question
+				theGame.textQuestion.setAnswerList(ANSWERS_THREE,ANSWER_FALSE);	// set the answer
+				theGame.textQuestion.initGui();									// call initGui form textQuestion class
 			}
 			break;
-		
-		
-			
 		case 4:	
-			theGame.incrementLevel();
-			System.out.println("Level Number in Level One: " + theGame.getLevel());
-			theGame.changeLayoutCard("FOREST");
+			theGame.incrementLevel();											// increment the level of the game
+			theGame.changeLayoutCard("FOREST");									// go back to the ForestPanel
 			break;
+		}
+	}
 	
+	/**
+	 * METHOD: Add the possible answer choices to each question's
+	 * List that holds it's possible answer choices.
+	 * @param holdsPossibleAnswers
+	 * @param option1
+	 * @param option2
+	 */
+	public void addPossibleAnswers(List<String> holdsPossibleAnswers, String option1, String option2){
+		holdsPossibleAnswers.add(option1);										// add first string parameter 
+		holdsPossibleAnswers.add(option2);										// add second string parameter
+	}
+		
+	/**
+	 * METHOD: This method calls the next question once the correct
+	 * answer is selected from the previous question.
+	 * @param none
+	 */
+	public void nextQuestion() {
+		// Once the correct answer is selected, increment currentQuestion
+		// and call doQuestion again.
+		if (theGame.textQuestion.questionCompleted == true) {
+			currentQuestion++;													// increment currentQuestion
+			doQuestion();														// call doQuestion
+		}
+		// Otherwise, it throws an error.
+		else {
+			JLabel warningImage = new JLabel(new ImageIcon(BOB_SRC));
+			JOptionPane.showMessageDialog(null, warningImage, "Question", JOptionPane.PLAIN_MESSAGE, null);
 		}
 	}
 }
